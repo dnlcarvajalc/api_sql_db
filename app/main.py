9dbd108f863ba3c9339a7460c391f4b621721853
dnlcarvajalc <dnlcarvajalc@gmail.com>
2025-09-08 23:04:33 -0500

    hello world sql db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/db-hello")
+def db_hello():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT 'Hello from the DB!'"))
+        message = result.scalar()
+    return {"message": message}
